5139a5210f1f5d57da0d116e39f2c4231d86c033
dasha <hrebtovadara@gmail.com>
2020-08-17 10:23:33 +0300

    add data  to localstorage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
+
 
 export default new Vuex.Store({
   state: {
@@ -132,5 +134,6 @@ export default new Vuex.Store({
     addNewComment(context, payload) {
       context.commit('addNewComment', payload)
     }
-  }
+  },
+  plugins: [createPersistedState()],
 });
